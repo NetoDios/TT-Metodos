7f37d0d83e4ecc5b100b60a3a956bea48d4362a7
SauceRSC <rodrisc2000@hotmail.com>
2021-03-01 22:12:36 -0600

    Ejercicio 1 y 2 por Rodrigo Saucedo

--- Actividad 2.1 - Tsurus Tuneados.scm
+++ Actividad 2.1 - Tsurus Tuneados.scm
@@ -1,3 +1,16 @@
+;EJERCICIO 1
+;Esta funcion de scheme lo que hace es convertir una temperatura farenheit a celcius
+; se usa la formula C=(5*(F-32))/9
 (define (farenheit-to-celcius f)
-  ('Temp)
+  (/ (* 5 (- f 32)) 9)
+)
+
+;EJERCICIO 2
+;Esta funcion de scheme lo que hace es ver el signo de un numero, devuelve -1 si es negativo, 1 si es positivo y 0 si es 0
+;Se hace huco de cond para hacer las 3 verificaciones
+(define (sign n)
+  (cond ((< n 0) -1)
+        ((> n 0) 1)
+        ((= n 0) 0)
+  )
 )
