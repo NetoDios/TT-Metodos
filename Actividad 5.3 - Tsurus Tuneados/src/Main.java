b7d9a9ee090a61e8a7e7cecf498571587a8af4e4
Rodrigo Saucedo <rodrisc2000@hotmail.com>
2021-06-04 14:00:29 -0500

    Interfaz con la seleccion de carpeta; migracion del main

--- Actividad 5.3 - Tsurus Tuneados/src/Main.java
+++ Actividad 5.3 - Tsurus Tuneados/src/Main.java
@@ -1,38 +1,16 @@
-import java.io.File;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.*;
 
 public class Main {
-    private static FileQueue archivos;
-    public static void main(String[] args) {
-        archivos = new FileQueue();
-        try {
-            int nHilos = 1;
-            String path="";
-            Scanner x = new Scanner(System.in);
-            path=x.nextLine();
-            fillQueue(path);
-            Hilos[] hilos = new Hilos[nHilos];
 
-            for (int i=0 ; i<nHilos ; i++) {
-                hilos[i] = new Hilos(i+1, archivos);
-                hilos[i].start();
-            }
-        } catch (NumberFormatException ex) {
-            System.out.println(ex.getMessage());
+    public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         }
-    }
-    private static void fillQueue(String path) {
-        File dir = new File(path);
-        File[] dirFiles = dir.listFiles();
-        if(dirFiles != null){
-            for(File file : dirFiles) {
-                String name = file.getName();
-                archivos.addFile(name);
-            }
+        catch(Exception e){
+            System.out.println(e.getMessage());
         }
+        Panel uiPanel = new Panel();
+        uiPanel.setVisible(true);
     }
+
 }
---
+++ Actividad 5.3 - Tsurus Tuneados/src/Panel.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class Panel extends JFrame{
+    private JButton Browse;
+    private JPanel rootPanel;
+    private JLabel labelPic;
+    private static FileQueue archivos;
+    public static long inicio;
+
+    public Panel() {
+        add(rootPanel);
+        setTitle("Actividad 5.3 - Tsurus Tuneados");
+        setSize(450,600);
+        int nHilos = 1;
+        Hilos[] hilos = new Hilos[nHilos];
+        archivos = new FileQueue();
+        ImageIcon tsuruTuneado = new ImageIcon("Tank.png");
+        labelPic.setIcon(tsuruTuneado);
+        labelPic.setSize(750,720);
+        labelPic.setText("");
+        Browse.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Browse.setEnabled(false);
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int response = chooser.showOpenDialog(rootPanel);
+                if(response == JFileChooser.APPROVE_OPTION){
+                    String path = chooser.getSelectedFile().getPath();
+                    try {
+                        fillQueue(path);
+                        inicio = System.currentTimeMillis();
+                        for (int i=0 ; i<nHilos ; i++) {
+                            hilos[i] = new Hilos(i+1, archivos);
+                            hilos[i].start();
+                        }
+                    } catch (NumberFormatException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
+            }
+        });
+    }
+    private static void fillQueue(String path) {
+        File dir = new File(path);
+        File[] dirFiles = dir.listFiles();
+        if(dirFiles != null){
+            for(File file : dirFiles) {
+                String name = file.getName();
+                archivos.addFile(name);
+            }
+        }
+    }
+
+
+}
